35c22a46cbf99c188f270940105962e037439d1c
Jenny Johnson <kippz07@users.noreply.github.com>
2017-07-13 21:56:24 +0100

    styling basically done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,17 +8,34 @@ class CarsController < ApplicationController
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.new(car_params)
+    @car.save
+    redirect_to @car
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to @car
   end
 
   def destroy
+    Car.find(params[:id]).destroy
+    redirect_to cars_path
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def car_params
+      params.require(:car).permit(:name, :engine, :year, :tyre_type, :tyre_manufacturer, :engine_config, :wins, :drivers_championship, :constructors_championship, :image, :weight)
+    end
+
 end
